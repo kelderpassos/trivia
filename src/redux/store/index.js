3c4cd1804f562e40f71c4e6a9d8ec87340b37e60
Kelder Passos <kelder.passos@gmail.com>
2022-05-24 16:07:27 -0300

    Criada a tela de login

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,6 +3,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+if (window.Cypress) {
+  window.store = store;
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  ));
 
 export default store;
