ce43d8c70348efc861c933cb24d0be3d0cef3ffd
Thariklz <thariklz19@gmail.com>
2022-06-06 20:26:07 -0300

    setup_page

--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Setup extends Component {
   render() {
     return (
-      <div>
+      <div className="setupContain">
         <h1 data-testid="settings-title">
           Configurações
         </h1>
